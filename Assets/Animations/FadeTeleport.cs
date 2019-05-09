2d62e08bb26d26e6e83dce4d65ae5c2fa9f222a7
Jethro Schoppenhorst <38409262+JSchoppe@users.noreply.github.com>
2019-05-08 20:47:17 -0500

    Revert "Revert "basic-dialogue-implemented""
    This reverts commit 40549af2adf340ca165d0eea1c9d963bccd499cf.

--- Assets/Animations/FadeTeleport.cs
+++ Assets/Animations/FadeTeleport.cs
@@ -7,7 +7,6 @@ public class FadeTeleport : StateMachineBehaviour
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Debug.Log("Called");
         ScreenTransition transition = FindObjectOfType<ScreenTransition>();
         transition.Transition();
     }
