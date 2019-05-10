99d8e9939287336d08b42ecb85cf5f066e9e4807
Three Eyed Venelope <threeeyedvenelope@gmail.com>
2019-05-10 12:54:57 -0500

    Add dialogue change - theoretically

--- Assets/Scripts/DialogueChange.cs
+++ Assets/Scripts/DialogueChange.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueChange : MonoBehaviour
 {
+    private Button button;
+    private GameObject player;
+    private Transform playerLocation;
+
+    [SerializeField] private Button currentButton;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
+        playerLocation = player.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ButtonChange();   
+    }
+
+    private void ButtonChange()
+    {
+        if(playerLocation)
+        {
+            playerLocation.position = new Vector3(32, 9, 0);
+
+        }
     }
 }
