4dd67737bc3933d2f86eba2f4f3bb9dbd45d5a71
Scott Little <scott.little@loop.colum.edu>
2019-05-10 13:08:33 -0500

    Added Dialogue and Menu Scripts

---
+++ Assets/Scenes/DialogueTriggerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTriggerScript : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("This is the dialogue panel that will appear when the collider is triggered.")]
+    private GameObject dialogue;
+
+    PlayerMovementController playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogue.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && dialogue.activeSelf == true)
+        {
+            dialogue.SetActive(false);
+            playerController.PlayerCanMove = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerController = collision.GetComponent<PlayerMovementController>();
+            playerController.PlayerCanMove = false;
+            dialogue.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    GameObject creditsPanel;
+
+    private void Start()
     {
-        
+        Cursor.visible = false;
+        creditsPanel = GameObject.Find("Credits Panel");
+        creditsPanel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (Input.GetButtonDown("Fire2"))
+            creditsPanel.SetActive(!creditsPanel.activeSelf);
     }
 }
