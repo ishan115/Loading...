849b62da6ee0fa927479b88d12e7663bd02bdcf9
Scott Little <scott.little@loop.colum.edu>
2019-05-10 13:19:24 -0500

    Updated Player Prefab

---
+++ Assets/Scripts/EndGameColliderScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// This is for a trigger volume that will end the game and ring the player to the main menu 
+/// </summary>
+public class EndGameColliderScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Return To Title Scene
+        SceneManager.LoadScene(0);
+    }
+}
